01618c6f513666b16536a1bfc29f3e7a977a5f53
iamlistless <98135875+iamlistless@users.noreply.github.com>
2023-09-24 15:28:58 -0700

    Create files and establish basic layout using CSS Grid

---
+++ script.js
@@ -0,0 +1,5 @@
+// create a variable that refers to the grid container div
+const gridContainer = document.querySelector('.gridContainer')
+
+//create another constant that refers to an array of colors
+const color = [];
